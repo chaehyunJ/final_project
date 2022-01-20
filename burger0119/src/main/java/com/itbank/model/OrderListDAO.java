a5c565ba716061359fd9f594b3d6edf771d1df1c
seongdongjo <wp203gl@naver.com>
2022-01-20 18:08:44 +0900

    seongdong0119

---
+++ burger0119/src/main/java/com/itbank/model/OrderListDAO.java
@@ -0,0 +1,21 @@
+package com.itbank.model;
+
+import java.util.HashMap;
+
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Select;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OrderListDAO {
+
+	@Select("select * from orderlist_table where "
+			+ "orderdate=(select max(orderdate) from orderlist_table) "
+			+ "and userid = #{ userid }")
+	OrderListDTO seletOrderList(String userid);
+
+	@Insert("insert into orderlist_table (menus, price, paystatus, orderstatus, total, count,userid,orderaddress,phone,requirement) "
+			+ "values(#{menus}, #{price}, #{paystatus}, #{orderstatus}, #{total}, #{count},#{userid},#{orderaddress},#{phone},#{requirement})")
+	int payment(HashMap<String, String> map);
+
+}
